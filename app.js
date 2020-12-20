eb8c9d7fdd5f1c68e4fedd55a11a52f1fa8389e9
Helen <paznachka@gmail.com>
2020-12-20 23:16:49 +0200

    added script for modal

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let sendBtn = document.querySelector(".send-btn");
+// send-form
+
+let sendBtn = document.querySelector(".contact-btn");
 let sendForm = document.querySelector(".contact-form");
 sendBtn.classList.add("off");
 
@@ -19,3 +21,25 @@ sendBtn.addEventListener("click", function() {
         showSendForm();
     } else hideSendForm();
 })
+
+// modal
+
+let modal = document.querySelector(".modal");
+let subscribeBtn =document.querySelector(".subscribe-btn");
+
+window.addEventListener("scroll", function() {
+    if((window.pageYOffset>(document.body.scrollHeight/2)) && (window.pageYOffset<(document.body.scrollHeight/3*2)) ) {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+    }
+})
+subscribeBtn.addEventListener("click", function() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+})
+
+console.log(document.body.scrollHeight/3*2)
+console.log(window.pageYOffset)
+console.log(modal)
+
+
